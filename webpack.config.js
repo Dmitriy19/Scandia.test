89d3dc560df567f70eb924ac6a78b7c345f949aa
Radchenko <RadchenkoAS@qulix.com>
2018-02-01 13:32:00 +0300

    Chenge footer

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
-//const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const config = {
 
     entry: './js/app.js',
@@ -13,19 +12,48 @@ const config = {
     module:{
         rules: [
 
-
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: [ 'babel-loader' ]
             },
 
+            //Компиляция в screen.css.
             {
                 test: /\.sass$/,
-                use: [ 'style-loader', 'css-loader', 'postcss-loader', 'sass-loader' ]
-            },
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                        // If you are having trouble with urls not resolving add this setting.
+                        // See https://github.com/webpack-contrib/css-loader#url
+                            url: false,
+                            minimize: false,
+                            sourceMap: true,
+                            importLoaders: 1
+                        }
+                    },
 
-            {
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+
+
+                    ]
+                })
+             },
+        {
                 test: /\.(png|jpg|gif)$/,
                 use: [
                     {
@@ -37,6 +65,7 @@ const config = {
                     }
                 ]
             },
+
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [{
@@ -47,14 +76,13 @@ const config = {
                     }
                 }]
             }
+
+
         ]
+
     },
     plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        })
-        //new ExtractTextPlugin('screen.css')
+        new ExtractTextPlugin('screen.css')
         //if you want to pass in options, you can do so:
         //new ExtractTextPlugin({
         //  filename: 'screen.css'
